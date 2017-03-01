51c97561375683bb0788ea2851fe6c4b77e54554
AchmadRifai <achmad.rifai.jowo.asli@gmail.com>
2017-03-01 16:10:23 +0700

    This First Commit

---
+++ ThePos/Db.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace ThePos.util{
+	public class Db{
+		private MySqlConnection c;
+
+		public Db(){
+			String host = "localhost";
+			String dbname = "thepos";
+			String user = "root";
+			String pass = "";
+		}
+	}
+}
